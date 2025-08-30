b3130fa3255d8213d2d3161c275ef9c480ccfbf3
SyedBilal61 <bilalkazmi61.bk@gmail.com>
2025-08-30 23:32:03 +0200

    Intial commit

---
+++ src/main/java/com/example/Employee.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Employee {
+
+    private String id;
+    private double salary;
+    private boolean paid;
+
+    public Employee(String id, double salary) {
+        this.id = id;
+        this.salary = salary;
+    }
+
+    //getters and setters...and possibly equals and hashCode
+}
---
+++ src/test/java/com/example/EmployeeManagerTest.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InOrder;
+import org.mockito.ArgumentCaptor;
