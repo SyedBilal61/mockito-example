115f3a345e5760f36b0617b5fc74fb3856e8dad9
SyedBilal61 <bilalkazmi61.bk@gmail.com>
2025-08-31 00:11:50 +0200

    Updating test file and also the other files remove old dependencies and adding new junit dependency

---
+++ src/main/java/com/example/EmployeeManager.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class EmployeeManager {
+
+    private EmployeeRepository employeeRepository;
+
+    public EmployeeManager(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+}
--- src/test/java/com/example/EmployeeManagerTest.java
+++ src/test/java/com/example/EmployeeManagerTest.java
@@ -1,16 +1,20 @@
-package com.example;
-
-import static org.assertj.core.api.Assertions.*;
-
+import static org.mockito.Mockito.*;
+import org.junit.Before;
 import org.junit.Test;
 
 public class EmployeeManagerTest {
 
+    private EmployeeManager employeeManager;
+    private EmployeeRepository employeeRepository;
+
+    @Before
+    public void setup() {
+        employeeRepository = mock(EmployeeRepository.class);
+        employeeManager = new EmployeeManager(employeeRepository);
+    }
     @Test
-    public void testPayEmployeesWhenNoEmployeesArePresent() {
-        EmployeeManager employeeManager = new EmployeeManager();
-        int numberOfPayments = employeeManager.payEmployees();
-        assertThat(numberOfPayments).isZero();
+    public void testPayEmployeesWhenOneEmployeeIsPresent() {
+        assertThat(employeeManager.payEmployees()).isEqualTo(1);
     }
 
 }
